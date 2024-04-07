bad9c41f04c5fd6749cc11b10a245a954c7aed4e
MarcosPaulo62 <markossylva62@gmail.com>
2024-04-07 13:20:30 -0300

    Criado endpoint que retorna informações de um usuário logado

--- src/backend/src/routes.ts
+++ src/backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./Controllers/CreateUser/createUserController";
 import { AuthUserController } from "./Controllers/AuthenticateUser/authUserController";
+import { UserDetailsController } from "./Controllers/UserDetailsController/userDetailsController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router()
 
@@ -9,5 +11,8 @@ router.post("/user", new CreateUserController().handle)
 
 //login user
 router.post("/session", new AuthUserController().handle)
+
+//user details
+router.get("/user", isAuthenticated, new UserDetailsController().handle)
 
 export {router}
