5a663e984e6c96e6c46a15785cea36c98e89849a
Ana Laura <alsrsilva@sga.pucminas.br>
2024-05-18 00:00:01 -0300

    Formulário alerta confirmação de medicação

--- src/frontend/app/components/notifications/nextMedication/nextMedication.tsx
+++ src/frontend/app/components/notifications/nextMedication/nextMedication.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import { AppMedicationContext, MedicationProps } from "@/app/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from './nextMedication.module.css';
+import Modal from "../../modal/modal";
+import ConfirmMedication from "../../confirmMedication/confirmMedication";
 
 export default function NextMedication() {
 
     const { medications } = useContext(AppMedicationContext);
     const currentHour = new Date().getHours();
+
+    const [isOpenRegisterModal, setIsOpenRegisterModal] = useState(false);
     
     // Função para transformar a string de horários em uma array de horários
     const transformTimeToTakeToArray = (time_to_take: string): string[] => {
@@ -53,19 +57,24 @@ export default function NextMedication() {
 
     const nextMedication = getNextMedication();
     return (
-        <div className={styles.container}>
-            <p><strong>Próximo medicamento a tomar</strong></p>
-            {nextMedication ? (
-                <div className={styles.medInfo}>
-                    <div className={styles.medication}>
-                        <h4>{nextMedication.name}</h4>
-                        <p>Horário: {nextMedication.time_to_take.split(',').join(' - ')}</p>
-                        {/* Renderize outras informações do medicamento conforme necessário */}
+        <>
+            <div className={styles.container}  onClick={() => setIsOpenRegisterModal(true)}>
+                <p><strong>Próximo medicamento a tomar</strong></p>
+                {nextMedication ? (
+                    <div className={styles.medInfo}>
+                        <div className={styles.medication}>
+                            <h4>{nextMedication.name}</h4>
+                            <p>Horário: {nextMedication.time_to_take.split(',').join(' - ')}</p>
+                            {/* Renderize outras informações do medicamento conforme necessário */}
+                        </div>
                     </div>
-                </div>
-            ) : (
-                <p>Nenhum medicamento a ser tomado em breve.</p>
-            )}
-        </div>
+                ) : (
+                    <p>Nenhum medicamento a ser tomado em breve.</p>
+                )}
+            </div>
+            <Modal isModalOpen={isOpenRegisterModal} onCloseModal={() => setIsOpenRegisterModal(false)} modalTitle="Lembrete">
+                <ConfirmMedication />
+            </Modal>
+        </>
     );
 }
