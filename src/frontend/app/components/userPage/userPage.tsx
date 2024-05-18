df7ae7f0657872dc89057c7d07b2f2b62200b587
Guilherme2703 <glimadefreitasgit2002@gmail.com>
2024-05-18 18:22:44 -0300

    Guilherme - integração dos dados na tela Meu perfil

--- src/frontend/app/components/userPage/userPage.tsx
+++ src/frontend/app/components/userPage/userPage.tsx
@@ -4,6 +4,7 @@ import { useSession } from 'next-auth/react'
 import styles from './userPage.module.css'
 
 
+
 type UserDetailsProps = {
     user:{
         id: string,
@@ -13,21 +14,26 @@ type UserDetailsProps = {
         phone: string,
         date_of_birth: string,
         gender: string,
-        created_at: string
+        created_at: string,
     }
 }
 export default function UsuarioComponent(data: UserDetailsProps){
-
     const userSession = useSession()
-    
+
     return(
         <main className={styles.main}>
             <h1 className={styles.top}>Informações pessoais</h1>
             
             <div className={styles.container}>
-                name: {userSession.data?.user.name}
-                email: {userSession.data?.user.email}
+                <p className={styles.dataText}>Nome: {data.user.name}</p> 
+                <p className={styles.dataText}>Data de nascimento: { data.user.date_of_birth.slice(0, 10).split('-').reverse().join('/')}</p>   
+                <p className={styles.dataText}>Gênero: {data.user.gender.replace("MALE", "Masculino").replace("FEMALE", "Feminino")}</p> 
+                <p className={styles.dataText}>E-mail: {data.user.email} </p> 
+                <p className={styles.dataText}>Número de Telefone: ({data.user.phone.slice(0, 2) + ") " + data.user.phone.slice(2)} </p> 
+                <p className={styles.dataText}>Estado: Minas Gerais </p> 
+                <p className={styles.dataText}>Cidade: Montes Claros </p> 
             </div>
         </main>
     )
 }
+
