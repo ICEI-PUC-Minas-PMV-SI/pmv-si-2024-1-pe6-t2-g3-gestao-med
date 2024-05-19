62e2dfdf47aac276c9adb719a914d7fcd2ff9f89
jamessonseren <jamesson_seren@hotmail.com>
2024-05-19 17:39:56 -0400

    removed secret

--- src/frontend/app/lib/openai/openai.config.ts
+++ src/frontend/app/lib/openai/openai.config.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// const open_ai_key = process.env.OPEN_AI_KEY as string
-const open_ai_key = 'sk-proj-gBaz15j6Z8qGZ4XRtbkJT3BlbkFJMNfhUrzfinPbp5y14UP4'
+const open_ai_key = process.env.OPEN_AI_KEY as string
+console.log({open_ai_key})
 const openai = new OpenAI({
     apiKey: open_ai_key,
     dangerouslyAllowBrowser: true,
